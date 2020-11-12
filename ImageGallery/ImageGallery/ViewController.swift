547beb60d62c30646e65fc5df3959803a1e6d654
Rober Garrido <garrido.vazquez.rober@gmail.com>
2020-11-12 10:47:03 +0100

    Initial Commit

---
+++ ImageGallery/ImageGallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageGallery
+//
+//  Created by Rober Garrido on 12/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
